982947af804f0c06b97f657303a8447fcf4ae603
ViktorStefanko <mr.v.stefanko@gmail.com>
2018-12-21 10:15:11 +0100

    Improved security. Key as parameter

--- bpmn_crawler/main_script.py
+++ bpmn_crawler/main_script.py
@@ -2,57 +2,56 @@ from src.db_handler import DbHandler
 from src.rep_crawler import RepCrawler
 from src.tree_crawler import TreeCrawler
 import src.other_functions as my_functions
+import sys
 
-# Path to the database
-db_dir = "database"
-db_name = "sqliteBPMN.db"
-db_path = db_dir + "/" + db_name
+if len(sys.argv) == 3:
+    GH_KEY = str(sys.argv[1]) + "&" + str(sys.argv[2])
+    print(GH_KEY)
+    # Path to the database
+    db_dir = "database"
+    db_name = "sqliteBPMN.db"
+    db_path = db_dir + "/" + db_name
 
-# Two DB tables for input and output
-log_rep_table = "log_repos"
-projects_bpmn = "projects_bpmn"
-GH_KEY = "client_id=ecde69e021a6a9361e5d&client_secret=0a07515f3d78268c89dea9d25baad5c195216945"
+    # Two DB tables for input and output
+    log_rep_table = "log_repos"
+    projects_bpmn = "projects_bpmn"
 
-# Instances of required classes
-db_handler = DbHandler()
-rep_crawler = RepCrawler()
-tree_crawler = TreeCrawler(projects_bpmn)
+    # Instances of required classes
+    db_handler = DbHandler()
+    rep_crawler = RepCrawler(GH_KEY)
+    tree_crawler = TreeCrawler(projects_bpmn)
 
-print("Connection to DB: \n")
-db_conn = db_handler.create_connection(db_path)
-min_id_query = "SELECT min(id) FROM " + log_rep_table + ";"
-max_id_query = "SELECT max(id) FROM " + log_rep_table + ";"
-min_id = db_handler.execute_query(db_conn, min_id_query, True)[0][0]
-max_id = db_handler.execute_query(db_conn, max_id_query, True)[0][0]
-print("min_id: " + str(min_id))
-print("max_id: " + str(max_id))
-if min_id != max_id:
-    step = 500
-    for size in range(min_id, max_id, step):
-        start = str(size)
-        end = str(min(size + step, max_id))
-        print("\nSelect between: " + start + " AND " + end)
-        query = "SELECT login, name FROM " + log_rep_table + " WHERE id BETWEEN " + start + " AND " + end + ";"
-        # repo_list is a list of (username, repository_name) tuples
-        repo_list = db_handler.execute_query(db_conn, query, True)
-        if repo_list:
-            print("Make tree: \n")
-            temp_dir = my_functions.make_dir("temp")
-            master_dir = my_functions.make_dir(temp_dir + "/master")
-            default_dir = my_functions.make_dir(temp_dir + "/default")
-            trees_dir = my_functions.make_dir(temp_dir + "/trees")
-            rep_crawler.traverse_gh_repositories(repo_list, master_dir, default_dir, trees_dir, GH_KEY)
+    print("Connection to DB: \n")
+    db_conn = db_handler.create_connection(db_path)
+    min_id_query = "SELECT min(id) FROM " + log_rep_table + ";"
+    max_id_query = "SELECT max(id) FROM " + log_rep_table + ";"
+    min_id = db_handler.execute_query(db_conn, min_id_query, True)[0][0]
+    max_id = db_handler.execute_query(db_conn, max_id_query, True)[0][0]
+    print("min_id: " + str(min_id))
+    print("max_id: " + str(max_id))
+    if min_id != max_id:
+        step = 2400
+        for size in range(min_id, max_id, step):
+            start = str(size)
+            end = str(min(size + step, max_id))
+            print("\nSelect between: " + start + " AND " + end)
+            query = "SELECT login, name FROM " + log_rep_table + " WHERE id BETWEEN " + start + " AND " + end + ";"
+            # repo_list is a list of (username, repository_name) tuples
+            repo_list = db_handler.execute_query(db_conn, query, True)
+            if repo_list:
+                print("Make tree: \n")
+                temp_dir = my_functions.make_dir("temp")
+                master_dir = my_functions.make_dir(temp_dir + "/master")
+                default_dir = my_functions.make_dir(temp_dir + "/default")
+                trees_dir = my_functions.make_dir(temp_dir + "/trees")
+                rep_crawler.traverse_gh_repositories(repo_list, master_dir, default_dir, trees_dir, GH_KEY)
 
-            print("Search in trees for bpmn files: \n")
-            trees_repo_list = tree_crawler.get_repo_list(trees_dir)
-            db_conn = db_handler.create_connection(db_path)
-            tree_crawler.search_files(db_conn, trees_repo_list, trees_dir, default_dir)
+                print("Search in trees for bpmn files: \n")
+                trees_repo_list = tree_crawler.get_repo_list(trees_dir)
+                db_conn = db_handler.create_connection(db_path)
+                tree_crawler.search_files(db_conn, trees_repo_list, trees_dir, default_dir)
 
-            # Remove temp directories
-            my_functions.remove_dir(master_dir)
-            my_functions.remove_dir(default_dir)
-            my_functions.remove_dir(trees_dir)
-
-            # Remove data from db-table repo_list
-            del_query = "DELETE FROM " + log_rep_table + " WHERE id BETWEEN " + start + " AND " + end + ";"
-            repo_list = db_handler.execute_query(db_conn, del_query, False)
+                # Remove temp directories
+                my_functions.remove_dir(master_dir)
+                my_functions.remove_dir(default_dir)
+                my_functions.remove_dir(trees_dir)
