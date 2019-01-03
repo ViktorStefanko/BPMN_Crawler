970657a9657f244bae79808070cb847a1fea3d93
ViktorStefanko <mr.v.stefanko@gmail.com>
2019-01-03 23:59:26 +0100

    Added comments to main_script.py

--- main_script.py
+++ main_script.py
@@ -4,16 +4,29 @@ from src.tree_crawler import TreeCrawler
 import src.other_functions as my_functions
 import sys
 
+"""
+This script runs the program, that does GitHub-Repository-Mining concerning BPMN-diagrams 
+It requires 3 parameter
+:param argv[1]: the number of program/DB (E.g.: 1, 2, ..)
+:param argv[2]: client_id of application to use the GitHub API
+                (authentication increases limit from 60 up to 5000 hits per hour)
+:param argv[3]: client_secret of application to use the GitHub API
+Results will be stored in database
+"""
 if len(sys.argv) == 4:
-    # Paths to the databases
+    # Paths to DB that contains (user_id, username, name of repository and status)
     db_source_path = "databases/log_repos" + str(sys.argv[1]) + ".db"
+    # Paths to DB, where will be stored results
     db_result_path = "databases/result_bpmn" + str(sys.argv[1]) + ".db"
+
+    # Directory where all temporary files will be stored
+    temp_dir = my_functions.make_dir("temp" + str(sys.argv[1]))
 
     # Two DB tables for input and output
     log_rep_table = "log_repos" + str(sys.argv[1])
     res_bpmn_table = "result_bpmn" + str(sys.argv[1])
 
-    # GH-key for max limit 5000/hour
+    # GH-key for max number of accesses: 5000/hour
     GH_KEY = str(sys.argv[2]) + "&" + str(sys.argv[3])
 
     # Instances of required classes
@@ -21,35 +34,35 @@ if len(sys.argv) == 4:
     rep_crawler = RepCrawler(GH_KEY)
     tree_crawler = TreeCrawler(res_bpmn_table)
 
-    print("Connection to DB: \n")
+    print("Connection to DB's")
     db_conn_source = db_handler.create_connection(db_source_path)
+    db_conn_result = db_handler.create_connection(db_result_path)
     min_id_query = "SELECT min(new_id) FROM " + log_rep_table + " WHERE status=0;"
     max_id_query = "SELECT max(new_id) FROM " + log_rep_table + " WHERE status=0;"
     min_id = db_handler.execute_query(db_conn_source, min_id_query, True)[0][0]
     max_id = db_handler.execute_query(db_conn_source, max_id_query, True)[0][0]
 
-    print("min_id: " + str(min_id))
-    print("max_id: " + str(max_id))
+    print("Search between min_id: " + str(min_id) + " and max_id: " + str(max_id))
     if min_id != max_id:
         step = 50
         for begin in range(min_id, max_id, step):
             start = str(begin)
             end = str(min(begin + step - 1, max_id))
-            print("\nSelect between: " + start + " AND " + end)
             query = "SELECT login, name FROM " + log_rep_table + " WHERE new_id BETWEEN " + start + " AND " + end + ";"
             # repo_list is a list of (username, repository_name) tuples
             repo_list = db_handler.execute_query(db_conn_source, query, True)
+
             if repo_list:
-                print("Make tree: \n")
-                temp_dir = my_functions.make_dir("temp" + str(sys.argv[1]))
                 master_dir = my_functions.make_dir(temp_dir + "/master" + str(sys.argv[1]))
                 default_dir = my_functions.make_dir(temp_dir + "/default" + str(sys.argv[1]))
                 trees_dir = my_functions.make_dir(temp_dir + "/trees" + str(sys.argv[1]))
+
+                print("\nGet json files from GH API")
+                # Get json files with repositories information and store them into temp subdirectories
                 rep_crawler.traverse_gh_repositories(repo_list, master_dir, default_dir, trees_dir, GH_KEY)
 
-                print("Search in trees for bpmn files: \n")
+                print("\nSearch for BPMN files")
                 trees_repo_list = tree_crawler.get_repo_list(trees_dir)
-                db_conn_result = db_handler.create_connection(db_result_path)
                 if not tree_crawler.search_files(db_conn_result, trees_repo_list, trees_dir, default_dir):
                     print("Exception in tree_crawler!")
                     break
@@ -58,11 +71,11 @@ if len(sys.argv) == 4:
                 my_functions.remove_dir(default_dir)
                 my_functions.remove_dir(trees_dir)
 
+                # Change status of searched repositories from 0 to 1
                 update_query = "UPDATE " + log_rep_table + " SET status = 1 WHERE new_id BETWEEN " + start + " AND " + end + ";"
-                #del_rows_query = "DELETE FROM " + log_rep_table + " WHERE new_id BETWEEN " + start + " AND " + end + ";"
                 if db_handler.execute_query(db_conn_source, update_query, False):
-                    print("Has updated between " + str(start) + " and " + str(end))
+                    print("Has updated between " + str(start) + " and " + str(end) + "\n")
                 else:
                     break
 else:
-    print("Usage: param1 - number of db; param2 - id; param3 - password")
+    print("Usage: arg1 - number of db; arg2 - GH_client_id; arg3 - GH_client_password")
